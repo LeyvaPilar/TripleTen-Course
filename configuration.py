5bca8085313f90952d16ec7cbf25f9d0142d8eeb
Pilar Leyva <pilarleyva@MacBook-Pro-de-Pilar.local>
2024-09-23 22:55:40 +0200

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-5989283c-1373-4f7c-a2b8-b7322a45f3a0.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
